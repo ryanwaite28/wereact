f2bd1b74ffa46ded10211f0f44ba83c01c93998e
Rashadtheone <rashad.hammonds0114@gmail.com>
2024-01-11 13:02:56 -0500

    readme update~!

--- 1_ecommerce/src/header.js
+++ 1_ecommerce/src/header.js
@@ -1,9 +1,32 @@
 import React from "react";
-
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import StoreMallDirectoryIcon from '@mui/icons-material/StoreMallDirectory';
+import SearchIcon from '@mui/icons-material/Search';
 function Header() {
     return (
-        <div>
-
+        <div className="header">
+            <div className='header__logo'>
+        <StoreMallDirectoryIcon className='header__logoImage' fontSize='large'/>
+        <h2><div className='headerLogoTitle'>eShop</div></h2>
+      </div>
+      <div className='header__search'> 
+          <input div className='header__searchInput' />
+          <SearchIcon />
+      </div>
+      <div className='header__nav'>
+        <div className='nav__item'>
+          <span className='nav__itemLineOne'>Hello Gues</span>
+          <span className='nav__itemLineTwo'>Sign In</span>
+        </div>
+        <div className='nav__item'>
+        <span className='nav__itemLineOne'>Your</span>
+        <span className='nav__itemLineTwo'>Shop</span>
+        </div>
+        <div className='nav__item'>
+        <ShoppingBasketIcon fontSize='large' /> 
+        <span className='nav__itemLineTwo'>0</span>
+        </div>
+      </div>
         </div>
     )
 }
